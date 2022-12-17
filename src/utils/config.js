fb16ad605f59d92b400c5526fd2a9913053f7c7c
EliteDev619 <elitedev619@gmail.com>
2022-12-18 02:07:09 +0300

    config

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,8 @@
 export const oktaAuthConfig = {
-    issuer: `https://${process.env.REACT_APP_OKTA_BASE_URL}/oauth2/default`,
-    clientId: process.env.REACT_APP_OKTA_CLIENT_ID,
+    issuer: `https://dev-00042327.okta.com/oauth2/default`,
+    // issuer: `https://${process.env.REACT_APP_OKTA_BASE_URL}/oauth2/default`,
+    clientId: '0oa7o54ve3d6eZnyj5d7',
+    // clientId: process.env.REACT_APP_OKTA_CLIENT_ID,
     redirectUri: `${window.location.origin}/login/callback`,
     scopes: ["openid", "profile", "email"],
     pkce: true,
